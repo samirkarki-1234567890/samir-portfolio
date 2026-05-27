474dddf74c1ee026938eb8e71dc2d65b64a01962
Samir Karki <samir.karki@edu.sait.ca>
2026-05-27 14:01:46 -0600

    deployment files added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export', // Tells Next.js to export static HTML instead of needing a server
+  basePath: '/samir-portfolio', // Fixes link routing for GitHub sub-folders
+  images: {
+    unoptimized: true, // Required because GitHub Pages cannot dynamically resize images
+  },
 };
 
 export default nextConfig;
